8594b558c64e6b76efa31d162417034d277a8395
Macauley Drake <macauleyann@gmail.com>
2018-10-03 20:00:37 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
